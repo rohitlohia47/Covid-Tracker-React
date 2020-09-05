1dc095ccc4710795c45e3f0e63c5bedec92d5509
rohitlohia47 <rohitlohia211@gmail.com>
2020-09-05 12:32:38 +0530

    Updates

---
+++ precache-manifest.d6b7b047b1f930a1ff8290315835ee9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1973d246ad2ccc235d391ee4181f2f",
+    "url": "/Covid-Tracker-React/index.html"
+  },
+  {
+    "revision": "7e886cdd6d4a6bc60966",
+    "url": "/Covid-Tracker-React/static/css/main.fd0acee1.chunk.css"
+  },
+  {
+    "revision": "0511bbb38d715c268fe3",
+    "url": "/Covid-Tracker-React/static/js/2.143d1d6f.chunk.js"
+  },
+  {
+    "revision": "3637834dbfd19d1cff89b16643d4f9ad",
+    "url": "/Covid-Tracker-React/static/js/2.143d1d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e886cdd6d4a6bc60966",
+    "url": "/Covid-Tracker-React/static/js/main.927b0059.chunk.js"
+  },
+  {
+    "revision": "de648fc4f46a663a3df1",
+    "url": "/Covid-Tracker-React/static/js/runtime-main.1be1b7fc.js"
+  },
+  {
+    "revision": "a6fafc4f3675bd41597e8cbc6d7aa457",
+    "url": "/Covid-Tracker-React/static/media/logo.a6fafc4f.png"
+  }
+]);
